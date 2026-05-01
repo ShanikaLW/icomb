b9c8329147d3dbc3267603954584b3b6c545cf18
ShanikaLW <shanika.wickramasuriya@gmail.com>
2026-05-02 00:19:16 +1000

    Remove reference url

--- R/reconciliation.R
+++ R/reconciliation.R
@@ -45,7 +45,7 @@
 #' @references Nguyen, M., Vahid, F., & Wickramasuriya, S. L. (2025).
 #' Hierarchical Forecasting: The Role of Information Combination (Working Paper No. 11/25).
 #' Department of Econometrics and Business Statistics, Monash University.
-#' URL: \url{https://www.monash.edu/business/ebs/research/publications/ebs/2025/wp11-2025.pdf}
+#'
 #' @export
 #'
 #' @examples
