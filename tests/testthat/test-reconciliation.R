197fa809091a5736c5219fe6bb6bb90d5334e7e5
ShanikaLW <shanika.wickramasuriya@gmail.com>
2026-05-04 00:02:40 +1000

    Skip tests on cran

--- tests/testthat/test-reconciliation.R
+++ tests/testthat/test-reconciliation.R
@@ -1,45 +1,48 @@
-tourism_gts <- tsibble::tourism |>
-  fabletools::aggregate_key(Purpose,
-                            Trips = sum(Trips))
+test_that("reconciliation", {
+  skip_on_cran()
+  tourism_gts <- tsibble::tourism |>
+    fabletools::aggregate_key(Purpose,
+                              Trips = sum(Trips))
 
-reconciled_fc <- tourism_gts |>
-  fabletools::model(base = fable::SNAIVE(Trips)) |>
-  fabletools::reconcile(icomb = icomb(base, train_size = 75)) |>
-  forecast()
+  reconciled_fc <- tourism_gts |>
+    fabletools::model(base = fable::SNAIVE(Trips)) |>
+    fabletools::reconcile(icomb = icomb(base, train_size = 75)) |>
+    forecast()
 
-agg_fc <- reconciled_fc |>
-  dplyr::filter(.model == "icomb", fabletools::is_aggregated(Purpose))
+  agg_fc <- reconciled_fc |>
+    dplyr::filter(.model == "icomb", fabletools::is_aggregated(Purpose))
 
-bottom_fc <- reconciled_fc |>
-  dplyr::filter(.model == "icomb", !fabletools::is_aggregated(Purpose)) |>
-  dplyr::summarise(.mean = sum(.mean))
+  bottom_fc <- reconciled_fc |>
+    dplyr::filter(.model == "icomb", !fabletools::is_aggregated(Purpose)) |>
+    dplyr::summarise(.mean = sum(.mean))
 
-expect_equal(agg_fc$.mean, bottom_fc$.mean)
+  expect_equal(agg_fc$.mean, bottom_fc$.mean)
 
-reconciled_fc <- tourism_gts |>
-  fabletools::model(base = fable::ETS(Trips)) |>
-  fabletools::reconcile(icomb = icomb(base, train_size = 75)) |>
-  forecast()
+  reconciled_fc <- tourism_gts |>
+    fabletools::model(base = fable::ETS(Trips)) |>
+    fabletools::reconcile(icomb = icomb(base, train_size = 75)) |>
+    forecast()
 
-agg_fc <- reconciled_fc |>
-  dplyr::filter(.model == "icomb", fabletools::is_aggregated(Purpose))
+  agg_fc <- reconciled_fc |>
+    dplyr::filter(.model == "icomb", fabletools::is_aggregated(Purpose))
 
-bottom_fc <- reconciled_fc |>
-  dplyr::filter(.model == "icomb", !fabletools::is_aggregated(Purpose)) |>
-  dplyr::summarise(.mean = sum(.mean))
+  bottom_fc <- reconciled_fc |>
+    dplyr::filter(.model == "icomb", !fabletools::is_aggregated(Purpose)) |>
+    dplyr::summarise(.mean = sum(.mean))
 
-expect_equal(agg_fc$.mean, bottom_fc$.mean)
+  expect_equal(agg_fc$.mean, bottom_fc$.mean)
 
-reconciled_fc <- tourism_gts |>
-  fabletools::model(base = fable::ETS(Trips)) |>
-  fabletools::reconcile(icomb = icomb(base, train_size = 75)) |>
-  forecast(bootstrap = TRUE, times = 500)
+  reconciled_fc <- tourism_gts |>
+    fabletools::model(base = fable::ETS(Trips)) |>
+    fabletools::reconcile(icomb = icomb(base, train_size = 75)) |>
+    forecast(bootstrap = TRUE, times = 500)
 
-agg_fc <- reconciled_fc |>
-  dplyr::filter(.model == "icomb", fabletools::is_aggregated(Purpose))
+  agg_fc <- reconciled_fc |>
+    dplyr::filter(.model == "icomb", fabletools::is_aggregated(Purpose))
 
-bottom_fc <- reconciled_fc |>
-  dplyr::filter(.model == "icomb", !fabletools::is_aggregated(Purpose)) |>
-  dplyr::summarise(.mean = sum(mean(Trips)))
+  bottom_fc <- reconciled_fc |>
+    dplyr::filter(.model == "icomb", !fabletools::is_aggregated(Purpose)) |>
+    dplyr::summarise(.mean = sum(mean(Trips)))
 
-expect_equal(mean(agg_fc$Trips), bottom_fc$.mean)
+  expect_equal(mean(agg_fc$Trips), bottom_fc$.mean)
+})
